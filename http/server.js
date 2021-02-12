5983ea9f0eadef2fd17d198163deec0c1e3d6a6c
Nadno <eunadno@gmail.com>
2021-02-12 18:59:20 -0300

    created http server

---
+++ http/server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+http
+  .createServer((req, res) => {
+    const file = req.url === '/'
+      ? 'index.html'
+      : req.url;
+    const filePath = path.join(__dirname, 'public', file);
+    const extName = path.extname(filePath);
+
+    const allowedFileType = ['.html', '.css', '.js'];
+    const allowed = allowedFileType.find(item => item === extName);
+
+    if (!allowed) return;
+
+    if (req.url === '/') {
+      fs.readFile(
+        filePath,
+        (err, content) => {
+          if (err) throw err;
+          return res.end(content);
+        }
+      );
+    }
+
+    return;
+  })
+  .listen(5000, () => console.log('Server is running'));
